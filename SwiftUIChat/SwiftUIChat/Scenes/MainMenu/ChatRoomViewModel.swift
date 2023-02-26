8a9aac80c0983a3eb4ad730622f3b35f58fe75d6
mohamedshiha15@gmail.com <mohamedshiha15@gmail.com>
2023-02-26 09:17:01 +0200

    Added delete room functionality.

--- SwiftUIChat/SwiftUIChat/Scenes/MainMenu/ChatRoomViewModel.swift
+++ SwiftUIChat/SwiftUIChat/Scenes/MainMenu/ChatRoomViewModel.swift
@@ -7,25 +7,30 @@
 
 import Foundation
 
-final class ChatRoomViewModel: ObservableObject {
+@MainActor final class ChatRoomViewModel: ObservableObject {
     
-    private let manager: any Managable
+    private let manager: any ChatRoomManagable
+	@Published var chatroom: ChatRoom? = nil
     
-    init(manager: some Managable) {
+    init(manager: some ChatRoomManagable) {
         self.manager = manager
     }
     
     /// Start a chat room for a specific duration in **minutes**.
-    func start(forDuration duration: Int) async {
-        let room = await manager.create(for: duration) as? ChatRoom
+    func start(forDuration duration: Int) async { 
+		let room = await manager.create(for: duration)
         if let room {
             print(room.description)
+			self.chatroom = room
         } else {
             print("Error Creating Room in Firestore.")
         }
     }
     
-    func delete() async {
-        print("Delete Room!")
+	func delete() async -> Bool {
+		guard let chatroom else {
+			return false
+		}
+		return await manager.delete(managed: chatroom)
     }
 }
