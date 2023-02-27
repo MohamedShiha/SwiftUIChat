efa49f60f075bbae69c13a5a5bcdd432d5bfc5c0
mohamedshiha15@gmail.com <mohamedshiha15@gmail.com>
2023-02-27 02:36:56 +0200

    Modified documentation.

--- SwiftUIChat/SwiftUIChat/FirestoreManager/ChatRoom/Managing.swift
+++ SwiftUIChat/SwiftUIChat/FirestoreManager/ChatRoom/Managing.swift
@@ -13,17 +13,8 @@ protocol Managing {
 	/// The type of the managed object
 	associatedtype Managed: Identifiable
 	
-	/**
-	
-	 Initiates an object based on its presence/existence in a remote env.
-	 
-	 If there is an id saved in UserDefaults,
-	 a chat room could be still alive in the remote server
-	 The room will exist based on its ttl or time-to-live then it is fetched.
-	 If there is NO id saved in UserDefaults then, a new room will be created.
-	 
-	 - Returns: The managed object in case of successful initiation.
-	 */
+	 /// Initiates an object based on its presence/existence in a remote env.
+	 /// - Returns: The managed object in case of successful initiation.
 	func initiate() async -> Managed?
 	
 	/// Create the object for a limited duration in **minutes** in a remote env.
