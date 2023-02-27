496ae2a5c2901ba3231961b3068bc858c73b5a4d
mohamedshiha15@gmail.com <mohamedshiha15@gmail.com>
2023-02-27 02:26:41 +0200

    Added room initiation based on room status and also added deleting room functionality.

--- SwiftUIChat/SwiftUIChat/FirestoreManager/ChatRoom/ChatRoomManager.swift
+++ SwiftUIChat/SwiftUIChat/FirestoreManager/ChatRoom/ChatRoomManager.swift
@@ -9,7 +9,7 @@ import Foundation
 import FirebaseFirestore
 import FirebaseFirestoreSwift
 
-final class ChatRoomManager: ChatRoomManagable {
+final class ChatRoomManager: ChatRoomManaging {
         
     typealias Managed = ChatRoom
     
@@ -17,11 +17,43 @@ final class ChatRoomManager: ChatRoomManagable {
     private var rooms: CollectionReference {
         return firestore.collection("chat-rooms")
     }
+	/// A hardcoded duration for a chat room in minutes
+	private let DURATION = 30
+	
+	func initiate() async -> ChatRoom? {
+		if let id = ChatRoom.fetchId() {
+			guard let room = await get(managedId: id) else {
+				return await create(for: DURATION)
+			}
+			let destroyDate = Date(timeIntervalSince1970: TimeInterval(room.createdAt + room.ttl))
+			// Check if now the room is still alive?
+			if Date() < destroyDate {
+				// Room is still valid
+				return room
+			} else {
+				// Room should be terminated
+				try? await delete(managed: room)
+				return await create(for: DURATION)
+			}
+		} else {
+			return await create(for: DURATION)
+		}
+	}
+	
+	func get(managedId: String) async -> ChatRoom? {
+		do {
+			return try await rooms.document(managedId).getDocument(as: ChatRoom.self)
+		} catch {
+			print(error.localizedDescription)
+			return nil
+		}
+	}
     
     func create(for duration: Int) async -> ChatRoom? {
         let room = ChatRoom(durationInMinutes: duration)
         do {
             try rooms.document(room.id).setData(from: room)
+			room.persistId()
             return room
         } catch {
             print(error.localizedDescription)
@@ -29,13 +61,8 @@ final class ChatRoomManager: ChatRoomManagable {
         }
     }
     
-	func delete(managed room: Managed) async -> Bool {
-		do {
-			try await rooms.document(room.id).delete()
-			return true
-		} catch {
-			print(error.localizedDescription)
-			return false
-		}
+	func delete(managed room: Managed) async throws {
+		room.deleteId()
+		try await rooms.document(room.id).delete()
     }
 }
