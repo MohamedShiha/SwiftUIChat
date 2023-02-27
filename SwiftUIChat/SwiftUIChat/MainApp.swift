274e5c33c2068046174076fbad6776620e04f928
mohamedshiha15@gmail.com <mohamedshiha15@gmail.com>
2023-02-27 08:51:14 +0200

    Configured routing.

--- SwiftUIChat/SwiftUIChat/MainApp.swift
+++ SwiftUIChat/SwiftUIChat/MainApp.swift
@@ -7,17 +7,30 @@
 
 import SwiftUI
 
-let currentUser = User(id: "32B2C22D-C680-482C-B3AB-3DB4D04BA096", name: "Mohamed Shiha", avatar: nil)
+private let START_AS_HOST = true
+
+var CURRENT_USER: User {
+	if START_AS_HOST {
+		// Sender
+		return User(id: "32B2C22D-C680-482C-B3AB-3DB4D04BA096", name: "Mohamed Shiha", avatar: nil)
+	}
+	// Reciever
+	return User(id: "32BABE2D-CAW0-48C5-A12B-AEB4D12BA096", name: "Morsy Soker", avatar: nil)
+}
 
 @main
 struct SwiftUIChatApp: App {
     
     @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
-    
+	    
     var body: some Scene {
         WindowGroup {
             NavigationView {
-                MainMenu()
+				if START_AS_HOST {
+					MainMenu()
+				} else {
+					JoinRoomView()
+				}
             }
         }
     }
